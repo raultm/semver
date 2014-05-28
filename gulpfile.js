6f66e004fe490b7884c1f5d689b2d244b0ed5212
Raul Tierno <raultm@gmail.com>
2014-05-28 23:55:55 +0200

    Gulp - Match lastTag with version Pattern to extract [tag, version, major, minor, patch]

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,29 @@
 var gulp = require('gulp');
 var execSync = require('exec-sync')
 
+// http://regex101.com/r/fT7bX6
+var versionRegexPattern = /^(.*(([\d]+)\.([\d]+)\.([\d]+)).*)-[\d]+-\S+/;
+
+
 gulp.task('default', function(){
-    var lastTag = execSync('git describe');
-    console.log(lastTag);
+    lastVersionInfo = getLastVersionInfo();
+    console.log(lastVersionInfo);
+    matchVersionInTagInfo('v1.4.26-draft-2-message');
 });
+
+function getLastVersionInfo(){
+    var lastTagInfo = execSync('git describe');
+    return matchVersionInTagInfo(lastTagInfo);
+}
+
+function matchVersionInTagInfo(tag){
+    var result = tag.match(versionRegexPattern);
+    if(!result){ return false; }
+    return {
+        'tag'     : result[1],
+        'version' : result[2],
+        'major'   : result[3],
+        'minor'   : result[4],
+        'patch'   : result[5]
+    };
+}
