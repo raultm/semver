e73ef7aed5e4237fd2e177bf06ab674137023560
Raul Tierno <raultm@gmail.com>
2014-05-31 16:19:34 +0200

    DEV - We are going to get version from Git Tag, so we need to match agains git describe result

--- autosemver.js
+++ autosemver.js
@@ -1,8 +1,18 @@
 autosemver = {
-	
-	matchTag: function(versionString){
+
+    // http://regex101.com/r/fT7bX6
+    versionRegexPattern: /^(.*(([\d]+)\.([\d]+)\.([\d]+)).*)-[\d]+-\S+/,
+    matchTagFromGitDescribe: function(versionString){
 		if(!versionString) { return false; }
-		return versionString;
+        var result = versionString.match(this.versionRegexPattern);
+        var tag = {
+            tag    : result[1],
+            version: result[2],
+            major  : result[3],
+            minor  : result[4],
+            patch  : result[5]
+        }
+		return tag;
 	}
 }
 
--- test/test.js
+++ test/test.js
@@ -1,15 +1,24 @@
 var assert = require("assert");
 var semver = require("../autosemver.js").autosemver;
-console.log(semver);
 
 describe('AutoSemver', function(){
-  describe('matchTag', function(){
-    it('should return false when empty value', function(){
-      assert.equal('', semver.matchTag());
-    });
-    
-    it('should return version if founded', function(){
-      assert.equal('0.1.0', semver.matchTag('0.1.0'));
+    describe('matchTag', function(){
+        it('should return false when empty value', function(){
+            assert.equal('', semver.matchTagFromGitDescribe());
+        });
+
+        it('should return version 0.1.0-5-md5commit if versionString is the same', function(){
+            assert.equal('0.1.0', semver.matchTagFromGitDescribe('0.1.0-5-md5commit').version);
+        })
+
+        it('should return object defining three attributes (major.minor.patch)', function(){
+            var tag = semver.matchTagFromGitDescribe('0.1.0-5-md5commit');
+            assert.equal('0.1.0', tag.version);
+            assert.equal('0', tag.major);
+            assert.equal('1', tag.minor);
+            assert.equal('0', tag.patch);
+        })
+
+
     })
-  })
 })
