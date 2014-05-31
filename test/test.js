155da4cc4ebc7eaca8b12c10be48439be77ee540
Raul Tierno <raultm@gmail.com>
2014-05-31 16:37:54 +0200

    DEV - Checking that regex is designed correctly

--- test/test.js
+++ test/test.js
@@ -9,15 +9,34 @@ describe('AutoSemver', function(){
 
         it('should return version 0.1.0-5-md5commit if versionString is the same', function(){
             assert.equal('0.1.0', semver.matchTagFromGitDescribe('0.1.0-5-md5commit').version);
-        })
+        });
 
         it('should return object defining three attributes (major.minor.patch)', function(){
-            var tag = semver.matchTagFromGitDescribe('0.1.0-5-md5commit');
-            assert.equal('0.1.0', tag.version);
-            assert.equal('0', tag.major);
-            assert.equal('1', tag.minor);
-            assert.equal('0', tag.patch);
-        })
+            var describeObject = semver.matchTagFromGitDescribe('0.1.0-5-md5commit');
+            assert.equal('0.1.0', describeObject.tag);
+            assert.equal('0.1.0', describeObject.version);
+            assert.equal('0', describeObject.major);
+            assert.equal('1', describeObject.minor);
+            assert.equal('0', describeObject.patch);
+        });
+
+        it('should return object even if version has a "v" prepend', function(){
+            var describeObject = semver.matchTagFromGitDescribe('v0.1.0-5-md5commit');
+            assert.equal('v0.1.0', describeObject.tag);
+            assert.equal('0.1.0', describeObject.version);
+        });
+
+        it('should return object even if version has an append', function(){
+            var describeObject = semver.matchTagFromGitDescribe('v0.1.0alfa-5-md5commit');
+            assert.equal('v0.1.0alfa', describeObject.tag);
+            assert.equal('0.1.0', describeObject.version);
+        });
+
+        it('should return object even if version has an append, even if the append has a -', function(){
+            var describeObject = semver.matchTagFromGitDescribe('v0.1.0-alfa-5-md5commit');
+            assert.equal('v0.1.0-alfa', describeObject.tag);
+            assert.equal('0.1.0', describeObject.version);
+        });
 
 
     })
