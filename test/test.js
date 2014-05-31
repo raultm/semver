889f38143a5a1b3ba7b7ba85ddb0fa32734e27c5
Raul Tierno <raultm@gmail.com>
2014-05-31 19:55:57 +0200

    DEV - Add mocha to package.json dependency and rewrite some tests

--- test/test.js
+++ test/test.js
@@ -7,7 +7,7 @@ describe('AutoSemver', function(){
             assert.equal('', semver.matchVersionTagFromGitDescribe());
         });
 
-        it('should detect [tag, version, major, minor, patch] values from git describe result 0.1.0-5-md5commit', function(){
+        it('should detect [tag, version, major, minor, patch] values from git describe result [0.1.0-5-md5commit]', function(){
             var describeObject = semver.matchVersionTagFromGitDescribe('0.1.0-5-md5commit');
             assert.equal('0.1.0', describeObject.tag);
             assert.equal('0.1.0', describeObject.version);
@@ -16,19 +16,19 @@ describe('AutoSemver', function(){
             assert.equal('0', describeObject.patch);
         });
 
-        it('should detect tag and version correctly even if tag has a prepend like "v" - v0.1.0-5-md5commit', function(){
+        it('should detect tag and version correctly even if tag has a prepend like "v" [v0.1.0-5-md5commit]', function(){
             var describeObject = semver.matchVersionTagFromGitDescribe('v0.1.0-5-md5commit');
             assert.equal('v0.1.0', describeObject.tag);
             assert.equal('0.1.0', describeObject.version);
         });
 
-        it('should detect tag even if version has an append v0.1.0alfa-5-md5commit', function(){
+        it('should detect tag even if version has an append [v0.1.0alfa-5-md5commit]', function(){
             var describeObject = semver.matchVersionTagFromGitDescribe('v0.1.0alfa-5-md5commit');
             assert.equal('v0.1.0alfa', describeObject.tag);
             assert.equal('0.1.0', describeObject.version);
         });
 
-        it('should return object even if version has an append, even if the append has a "-" v0.1.0-alfa-5-md5commit', function(){
+        it('should detect tag even if version has an append, even if the append has a "-" [v0.1.0-alfa-5-md5commit]', function(){
             var describeObject = semver.matchVersionTagFromGitDescribe('v0.1.0-alfa-5-md5commit');
             assert.equal('v0.1.0-alfa', describeObject.tag);
             assert.equal('0.1.0', describeObject.version);
