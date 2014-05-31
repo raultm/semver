2b242b9b7c506c1f6e64d43826df6d94256e8750
Raul Tierno <raultm@gmail.com>
2014-05-31 23:03:35 +0200

    DEV - Refactor to change descriptionObject to tagObject

--- test/test.js
+++ test/test.js
@@ -20,30 +20,30 @@ describe('AutoSemver', function(){
         });
 
         it('should detect [tag, version, major, minor, patch] values from git describe result [0.1.0-5-md5commit]', function(){
-            var describeObject = semver.matchVersionTagFromGitDescribe('0.1.0-5-md5commit');
-            assert.equal('0.1.0', describeObject.tag);
-            assert.equal('0.1.0', describeObject.version);
-            assert.equal('0', describeObject.major);
-            assert.equal('1', describeObject.minor);
-            assert.equal('0', describeObject.patch);
+            var tagObject = semver.matchVersionTagFromGitDescribe('0.1.0-5-md5commit');
+            assert.equal('0.1.0', tagObject.tag);
+            assert.equal('0.1.0', tagObject.version);
+            assert.equal('0', tagObject.major);
+            assert.equal('1', tagObject.minor);
+            assert.equal('0', tagObject.patch);
         });
 
         it('should detect tag and version correctly even if tag has a prepend like "v" [v0.1.0-5-md5commit]', function(){
-            var describeObject = semver.matchVersionTagFromGitDescribe('v0.1.0-5-md5commit');
-            assert.equal('v0.1.0', describeObject.tag);
-            assert.equal('0.1.0', describeObject.version);
+            var tagObject = semver.matchVersionTagFromGitDescribe('v0.1.0-5-md5commit');
+            assert.equal('v0.1.0', tagObject.tag);
+            assert.equal('0.1.0', tagObject.version);
         });
 
         it('should detect tag even if version has an append [v0.1.0alfa-5-md5commit]', function(){
-            var describeObject = semver.matchVersionTagFromGitDescribe('v0.1.0alfa-5-md5commit');
-            assert.equal('v0.1.0alfa', describeObject.tag);
-            assert.equal('0.1.0', describeObject.version);
+            var tagObject = semver.matchVersionTagFromGitDescribe('v0.1.0alfa-5-md5commit');
+            assert.equal('v0.1.0alfa', tagObject.tag);
+            assert.equal('0.1.0', tagObject.version);
         });
 
         it('should detect tag even if version has an append, even if the append has a "-" [v0.1.0-alfa-5-md5commit]', function(){
-            var describeObject = semver.matchVersionTagFromGitDescribe('v0.1.0-alfa-5-md5commit');
-            assert.equal('v0.1.0-alfa', describeObject.tag);
-            assert.equal('0.1.0', describeObject.version);
+            var tagObject = semver.matchVersionTagFromGitDescribe('v0.1.0-alfa-5-md5commit');
+            assert.equal('v0.1.0-alfa', tagObject.tag);
+            assert.equal('0.1.0', tagObject.version);
         });
 
 
