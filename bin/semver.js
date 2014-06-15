3a9eb0acdf2a3a1ba99918039a5fcc7be067d586
Raul Tierno <raultm@gmail.com>
2014-06-15 16:11:42 +0200

    BIN - Debug before clirun

--- bin/semver.js
+++ bin/semver.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
-var semver = require('../index.js');
-semver.clirun(process.argv, process.cwd());
 console.log(process.argv);
 console.log(process.cwd());
+
+var semver = require('../index.js');
+semver.clirun(process.argv, process.cwd());
